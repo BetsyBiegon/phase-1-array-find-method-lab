953982788ea89579f1220d2427517e6166251e90
BetsyBiegon <betsybiegon@gmail.com>
2024-03-25 14:23:25 +0300

    code solution

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function superbowlWin(record) {
+    // Use the find() method to search for a win in the record
+    const winRecord = record.find((game) => {
+      return game.result === "W";
+    });
+  
+    // If a win is found, return the year
+    if (winRecord) {
+      return winRecord.year;
+    } else {
+      // If no win is found, return undefined
+      return undefined;
+    }
+  }
+  
